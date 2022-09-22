2e703e63bba06e473c22506048b49b08a5a320f1
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-09-22 12:03:30 +0800

    Add files via upload

---
+++ testDemo.java
@@ -0,0 +1,32 @@
+package linklist;
+
+public class testDemo {
+    //定义节点
+    public static class ListNode{
+        int val;
+        ListNode next;// 指针，指向下一个节点位置。
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    // 定义一个头节点
+    ListNode head;
+    // 定义一个尾节点
+    ListNode tail;
+    // 链表长度
+    int size;
+
+    // 增加节点，尾插，就是顺序插入
+    public void addNode(int val){
+        // 头节点的处理
+        if (head == null) {
+            head = new ListNode(val);
+            size++;
+        }
+
+    }
+
+
+}
