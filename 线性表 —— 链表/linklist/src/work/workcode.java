062b0ead06f04e6c1b502d5e4bf0af5a574e2667
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-09-29 20:02:50 +0800

    Add files via upload

--- 线性表 —— 链表/linklist/src/work/workcode.java
+++ 线性表 —— 链表/linklist/src/work/workcode.java
@@ -1,33 +1,30 @@
 package work;
 
-import jdk.nashorn.internal.ir.WhileNode;
-
-import java.util.Scanner;
 
 public class workcode {
     public static void main(String[] args) {
-        int arr[] = {2,34,9,24,5,524,23,82,341,8,6,2,3,4,32,4};
+        int arr[] = {2,34,9,24,5,524,23,82,341,8,6,8,3,4,32,4,2};
         workcode.LinkListClass lc = new workcode.LinkListClass();
         lc.creatlistT(arr);
         System.out.println("最大值为；"+lc.max(arr));
         System.out.println("请输入，想要查询的数：");
-//        Scanner scanner = new Scanner(System.in);
-//        int val = scanner.nextInt();
-        System.out.println(lc.amount(arr,2));
-//        scanner.close();
+        System.out.println(lc.amount(2));
         System.out.println("删除后为：");
-//        LinkNode p = lc.deleteDuplicates();
-//        while (true) {
-//            try {
-//                System.out.print(p.val+",");
-//                p=p.next;
-//            } catch (Exception e) {
-//                break;
-//            }
-//        }
-        System.out.println(lc.Delete());
-
+        LinkNode p = lc.Delete(arr);
+        while (true) {
+            try {
+                System.out.print(p.val+",");
+                p=p.next;
+                if(p.val == 0){
+                    System.out.println(p.val);
+                }
+            } catch (Exception e) {
+                break;
+            }
+        }
     }
+
+
     static class LinkNode {
         // 存储具体数据
         int val;
@@ -42,18 +39,17 @@ public class workcode {
         }
     }
 
+
+
     public static class LinkListClass{
         LinkNode head;
         LinkNode head2;
-        LinkNode head3;
         LinkNode tail;
         public LinkListClass(){
             head = new LinkNode();
             head.next = null;
             head2 = new LinkNode();
             head2.next = null;
-            head3 = new LinkNode();
-            head3.next = null;
             tail = new LinkNode();
             tail.next = null;
         }
@@ -102,9 +98,8 @@ public class workcode {
             return max1;
         }
 
-        public int amount(int a[],int X){
+        public int amount(int X){
             int total = 0;
-            creatlistF(a);
             LinkNode p = head;
             while (true) {
                 try {
@@ -112,7 +107,6 @@ public class workcode {
                     if (p.val == X) {
                         total++;
                     }
-                    System.out.println("     "+p.val+"     "+total);
                 } catch (Exception e) {
                     break;
                 }
@@ -120,77 +114,29 @@ public class workcode {
             return total;
         }
 
-
-
-//        public LinkNode deleteDuplicates () {
-//            // write code here
-//            if (head == null) return null;
-//
-//            LinkNode newHead = new LinkNode(0);
-//            newHead.next = head;
-//            LinkNode p = head, pre = newHead;
-//
-//            while (p != null && p.next != null) {
-//                // 关键：找到重复元素的起点
-//                if (p.val == p.next.val) {
-//                    LinkNode temp = p.next;
-//                    while (temp != null && temp.val == p.val) {
-//                        temp = temp.next;
-//                    }
-//                    pre.next = temp;
-//                    p = temp;
-//                } else {
-//                    pre = p;
-//                    p = p.next;
-//                }
-//            }
-//            return newHead.next;
-//        }
-
-
-
-
-
-
-
-
-
-
-        public int Delete(){
+        public LinkNode Delete(int a[]){
             LinkNode p = head;
             LinkNode l = p;
             int num = 0;
-//            LinkNode s = head2;
-            while (true) {
+            for (int i = 0; i < a.length; i++) {
                 boolean isTrue;
-                try {
-                    p = p.next;
-                    //声明一个标记，并每次重置
-                    isTrue = true;
-                    l = p;
-                    while (true) {
-                        try {
-                            l = l.next;
-                            if (p.val == l.val) {
-                                isTrue = false;
-                                break;
-                            }
-                        } catch (Exception e) {
-                            break;
-                        }
-                    }/*while*/
-                } catch (Exception e) {
-                    break;
+                p = p.next;
+                //声明一个标记，并每次重置
+                isTrue = true;
+                l = p;
+                for (int j = i+1; j < a.length; j++) {
+                    l = l.next;
+                    if (p.val == l.val) {
+                        isTrue = false;
+                        break;
+                    }
                 }
                 if (isTrue) {
-//                    s = new LinkNode(p.val);
-//                    s.next = head2.next;
-//                    head2.next = s;
-//                    creatlistself(p.val);
-                   num=p.val;
+                    creatlistself(p.val);
+                    num=p.val;
                 }
-            }/*while*/
-            return num;
+            }
+            return head2.next;
         }
 
     }
