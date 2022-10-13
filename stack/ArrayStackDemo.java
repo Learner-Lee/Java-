32c9eb2a056ed5e0930777d5fe0e072c7eb18a7a
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-10-13 10:12:05 +0800

    Add files via upload

--- stack/ArrayStackDemo.java
+++ stack/ArrayStackDemo.java
@@ -1,8 +1,52 @@
 package stack;
 
+import java.util.Scanner;
+
 public class ArrayStackDemo {
     public static void main(String[] args) {
+        // 测试 ArrayStack，是否正确。
 
+        // 先创建一个 ArrayStack 的对象。-> 表示栈
+        ArrayStack stack = new ArrayStack(4);
+        String key = "";
+        boolean loop = true;  // 控制是否退出菜单。
+        Scanner scanner = new Scanner(System.in);
+
+        while (loop){
+            System.out.println("show: 显示栈内信息");
+            System.out.println("exit: 退出栈");
+            System.out.println("push: 添加数据到栈");
+            System.out.println("pop : 从栈取出数据");
+            System.out.println("请输入你的选择：");
+            key = scanner.next();
+
+            switch (key) {
+                case "show":
+                    stack.list();
+                    break;
+                case "exit":
+                    scanner.close();
+                    loop = false;
+                    break;
+                case "push":
+                    System.out.println("请输入一个数：");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case "pop":
+                    try {
+                        int result = stack.pop();
+                        System.out.printf("出栈的数据为：%d\n",result);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+
+                default:
+                    break;
+            }/*switch*/
+        }/*while*/
+        System.out.println("程序已退出~~");
     }
 }
 // 定义一个 ArrayStack类，表示栈。
@@ -56,7 +100,7 @@ class ArrayStack{
             return;
         }
         for (int i = top; i >= 0; i--) {
-            System.out.printf("stack[%d] = %d",i,stack);
+            System.out.printf("stack[%d] = %d\n",i,stack[i]);
         }
         
     }
