0bf2218eb696372b12a99c83da959e42f27c7618
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-11-22 11:24:26 +0800

    Add files via upload

--- HuffmanTree/HuffmanTree.java
+++ HuffmanTree/HuffmanTree.java
@@ -1,15 +1,36 @@
 package HuffmanTree;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class HuffmanTree {
     public static void main(String[] args) {
         int[] arr = {13,7,8,3,29,6,1};
+        Node root = createHuffmanTree(arr);
+
+        //测试
+        preOrder(root);
+    }
+
+    //编写一个前序遍历方法
+    public static void preOrder(Node root){
+        if (root != null) {
+            root.PreOrder();
+        }else {
+            System.out.println("空树~");
+        }
     }
 
     // 创建哈夫曼树的方法
-    public static void createHuffmanTree(int[] arr){
+
+    /**
+     *
+     * @param arr 需要创建成哈夫曼树的数组
+     * @return 创建好后的哈夫曼树的root值
+     */
+    public static Node createHuffmanTree(int[] arr){
         // 第一步为了操作方便
         // 1. 遍历 arr 数组
         // 2. 将arr的每个元素构成成一个Node
@@ -18,6 +39,33 @@ public class HuffmanTree {
         for (int value : arr) {
             nodes.add(new Node(value));
         }
+
+        while (nodes.size() > 1){
+            // 排序从小到达
+            Collections.sort(nodes);
+
+            System.out.println("node = " + nodes);
+
+            // 取出根节点权值最小的两个二叉树
+            //(1) 取出权值最小的结点
+            Node leftnode = nodes.get(0);
+            //(2) 取出权值第二小的结点
+            Node rightnode = nodes.get(1);
+
+            //(3) 构造一棵新的二叉树
+            Node parent = new Node(leftnode.value + rightnode.value);
+            parent.left = leftnode;
+            parent.right = rightnode;
+
+            //(4) 从ArrayList删除处理过的二叉树
+            nodes.remove(leftnode);
+            nodes.remove(rightnode);
+            //(5) 将parent加入到nodes中
+            nodes.add(parent);
+        }
+
+        return nodes.get(0);
+
     }
 }
 
@@ -32,6 +80,17 @@ class Node implements Comparable<Node>{
 
     public Node(int value) {
         this.value = value;
+    }
+
+    // 前序遍历
+    public void PreOrder(){
+        System.out.println(this);
+        if (this.left != null) {
+            this.left.PreOrder();
+        }
+        if (this.right != null) {
+            this.right.PreOrder();
+        }
     }
 
     @Override
