29b81395794d776bace54f9994225c4dab795366
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2023-01-16 11:36:12 +0800

    Add files via upload

---
+++ leetcode/src/NewBegin/Dayfive.java
@@ -0,0 +1,47 @@
+package NewBegin;
+
+public class Dayfive {
+    public static void main (String[] args) {
+        Solution s = new Solution();
+        String str = s.convert("PAYPALISHIRING",3);
+        System.out.println(str);
+    }
+}
+
+class Solution {
+    public String convert (String s,int numRows) {
+        char[] a = s.toCharArray();
+        char[][] inside = new char[numRows][s.length()];
+        String str = "";
+        int m = 0;
+        int n = 0;
+//        System.out.println(a[m]);
+
+        while (m < a.length) {
+            for (int i = 0; i < inside.length; i++) {
+                if (m >= a.length) {
+                    break;
+                }
+                inside[i][n] = a[m];
+                m++;
+            }
+            n++;
+            for (int i = inside.length - 2; i > 0; i--) {
+                if (m >= a.length) {
+                    break;
+                }
+                inside[i][n] = a[m];
+                m++;
+                n++;
+            }
+        }
+        for (int i = 0; i < inside.length; i++) {
+            for (int j = 0; j < inside[i].length; j++) {
+                if (inside[i][j] != '\u0000') {
+                    str = str + inside[i][j];
+                }
+            }
+        }
+        return str;
+    }
+}
